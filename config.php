cdd4a9fae52391177679053a3fc41068ee6c9284
tthomas <talson@algorythma.com>
2018-11-04 20:21:29 +0400

    cleanup

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mysqli';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'mysql.marathon.l4lb.thisdcos.directory';
+$CFG->dbname    = 'moodle';
+$CFG->dbuser    = 'moodledude';
+$CFG->dbpass    = 'P@ssw0rd';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => 3306,
+  'dbsocket' => '',
+  'dbcollation' => 'utf8_unicode_ci',
+);
+
+$CFG->wwwroot   = 'http://192.168.71.31:10104/moodle';
+$CFG->dataroot  = '/var/moodledata';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
